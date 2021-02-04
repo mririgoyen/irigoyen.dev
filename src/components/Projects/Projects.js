697af80078ca3ebfefc5bf717fd232558ace6b90
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-04 11:00:00 -0600

    Add Projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,8 @@
 import SectionContainer from '../SectionContainer/SectionContainer';
 
+import PictogrammersWebp from '../../assets/images/pictogrammers.webp';
+import PictogrammersPng from '../../assets/images/pictogrammers.png';
+
 import classes from './Projects.scss';
 
 const Projects = ({ reportVisibility }) => {
@@ -10,7 +13,35 @@ const Projects = ({ reportVisibility }) => {
       id='projects'
       reportVisibility={reportVisibility}
     >
-      <h1>Projects</h1>
+      <div className={classes.container}>
+        <header>
+          <h1>Projects</h1>
+        </header>
+        <div className={classes.items}>
+          <div className={classes.item}>
+            <figure className={classes.pictogrammers}>
+              <picture>
+                <source srcset={PictogrammersWebp} type='image/webp' />
+                <source srcset={PictogrammersPng} type='image/png' />
+                <img
+                  alt='Pictogrammers'
+                  height={275}
+                  loading='lazy'
+                  src={PictogrammersPng}
+                  width={275}
+                />
+              </picture>
+            </figure>
+            <div className={classes.info}>
+              <h2>Pictogrammers</h2>
+              <h3>
+                <em>Iconography for Developers</em>
+              </h3>
+              <p><a href='https://github.com/Pictogrammers'>Pictogrammers</a> is an open-source community developing high-quality icons for use by designers and developers in web and application development. As a core contributor, I help maintain our icon databases, <a href='https://github.com/Templarian/MaterialDesign/issues'>field issues on GitHub</a>, assist in creation and maintenance of our supporting websites, and have personally contributed more than 1000 icons to our <a href='https://materialdesignicons.com/'>Material Design Icons</a> library!</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </SectionContainer>
   );
 };
--- src/hooks/useConfig.js
+++ src/hooks/useConfig.js
@@ -2,7 +2,7 @@ import { mdiHome } from '@mdi/js';
 
 import About from '../components/About/About';
 import Resume from '../components/Resume/Resume';
-// import Projects from '../components/Projects/Projects';
+import Projects from '../components/Projects/Projects';
 import Talks from '../components/Talks/Talks';
 import Philanthropy from '../components/Philanthropy/Philanthropy';
 import Contact from '../components/Contact/Contact';
@@ -13,7 +13,7 @@ const useConfig = () => {
       { icon: mdiHome, id: 'home', name: 'Home' },
       { component: About, id: 'about', name: 'About' },
       { component: Resume, id: 'resume', name: 'Resume' },
-      // { component: Projects, id: 'projects', name: 'Projects' },
+      { component: Projects, id: 'projects', name: 'Projects' },
       { component: Talks, id: 'talks', name: 'Talks' },
       { component: Philanthropy, id: 'philanthropy', name: 'Philanthropy' },
       { component: Contact, id: 'contact', name: 'Contact' }
