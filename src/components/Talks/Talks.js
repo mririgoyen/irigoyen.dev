cb1976dbe4a1f3bb9db44b4408a0d4a905f04980
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-04 08:42:01 -0600

    Talk description change

--- src/components/Talks/Talks.js
+++ src/components/Talks/Talks.js
@@ -51,7 +51,7 @@ const Talks = ({ reportVisibility }) => {
                 <span className={classes.bullet}>&bull;</span>
                 <span>October 9, 2019</span>
               </h3>
-              <p>Good APIs can level-up the dev life, but great APIs can bring an "Extra Life" to kids in need. In this talk, we discuss building a JavaScript API that can be used in 3 different ways from the same source code with the goal of making it easier to help raise money for sick kids in our communities via the Extra-Life charity.</p>
+              <p>Good APIs can level-up the dev life, but great APIs can bring an "Extra Life" to kids in need. In this talk, we discuss building a JavaScript API that can be used in 3 different ways from the same source code with the goal of making it easier to help raise money for sick kids in our communities via the Extra Life fundraiser.</p>
               <a href='/presentations/1up-empowering-communities-with-apis'>
                 <Icon path={mdiArrowDown} size={.9} />
                 Download Slide Deck
