7fcb959124fe67ebbab2aea5cc08b069f2322a26
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-07 22:48:08 -0600

    Back out react-helmet changes and insert version number via webpack build

--- preact.config.js
+++ preact.config.js
@@ -1,12 +1,20 @@
 import path from 'path';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
+import { version } from './package.json';
+
 export default {
   webpack(config, env, helpers) {
-    const cssClassIdentName = process.env.NODE_ENV === 'development' ? '[path][name]__[local]' : '[hash:base64:5]';
-
     // Override CSS modules class names
+    const cssClassIdentName = process.env.NODE_ENV === 'development' ? '[path][name]__[local]' : '[hash:base64:5]';
     config.module.rules[4].use[1].options.modules.localIdentName = cssClassIdentName;
+
+    // Inject the GitHub build ID
+    const [ major, minor ] = version.split('.');
+    const { plugin: htmlPlugin } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0];
+    Object.assign(htmlPlugin.options.meta, {
+      version: process.env.GITHUB_RUN_ID ? `${major}.${minor}.${process.env.GITHUB_RUN_ID}` : 'dev'
+    });
 
     // Copy assets to the root
     config.plugins.push(
@@ -17,9 +25,5 @@ export default {
         }]
       })
     );
-
-    // Assign variables for use in client code
-    const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
-    Object.assign(plugin.definitions, { 'process.env.GITHUB_RUN_ID': process.env.GITHUB_RUN_ID || '"dev"' });
   }
 };
