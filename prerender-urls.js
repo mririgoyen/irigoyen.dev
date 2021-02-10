34011f34354798f91f0bf83695ea5256963ca2a8
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-10 08:33:55 -0600

    Attempt to prerender the catchall 404 page

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,30 +1,10 @@
 module.exports = () => ([
-  {
-    title: 'Michael Irigoyen - Front-End Software Engineer',
-    url: '/'
-  },
-  {
-    title: 'Michael Irigoyen - Downloading Michael Irigoyen.pdf',
-    url: '/resume'
-  },
-  {
-    title: 'Michael Irigoyen - Downloading 1UP: Empowering Communities with APIs.pptx',
-    url: '/presentations/1up-empowering-communities-with-apis'
-  },
-  {
-    title: 'Michael Irigoyen - Downloading Design Your API for Humans.pptx',
-    url: '/presentations/design-your-api-for-humans'
-  },
-  {
-    title: 'Michael Irigoyen - Downloading The API User Experience.pptx',
-    url: '/presentations/the-api-user-experience'
-  },
-  {
-    title: 'Michael Irigoyen - Downloading Continuous DevOps.pptx',
-    url: '/presentations/continuous-devops'
-  },
-  {
-    title: 'Michael Irigoyen - Downloading Nobody Cares About Your UI.pptx',
-    url: '/presentations/nobody-cares-about-your-ui'
-  }
+  { url: '/' },
+  { url: '/resume' },
+  { url: '/presentations/1up-empowering-communities-with-apis' },
+  { url: '/presentations/design-your-api-for-humans' },
+  { url: '/presentations/the-api-user-experience' },
+  { url: '/presentations/continuous-devops' },
+  { url: '/presentations/nobody-cares-about-your-ui' },
+  { url: '*' }
 ]);
