803707269c5c35431053d49a7090721d9e1925be
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-10 08:54:04 -0600

    Attempt to prerender 404 page

--- prerender-urls.js
+++ prerender-urls.js
@@ -5,5 +5,6 @@ module.exports = () => ([
   { url: '/presentations/design-your-api-for-humans' },
   { url: '/presentations/the-api-user-experience' },
   { url: '/presentations/continuous-devops' },
-  { url: '/presentations/nobody-cares-about-your-ui' }
+  { url: '/presentations/nobody-cares-about-your-ui' },
+  { url: '/404' }
 ]);
