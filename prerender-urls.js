11badb9c69851e4471b87e010b82ff7670c8cfe9
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-10 08:23:34 -0600

    Take first pass at prerendering URLs

---
+++ prerender-urls.js
@@ -0,0 +1,30 @@
+module.exports = () => ([
+  {
+    title: 'Michael Irigoyen - Front-End Software Engineer',
+    url: '/'
+  },
+  {
+    title: 'Michael Irigoyen - Downloading Michael Irigoyen.pdf',
+    url: '/resume'
+  },
+  {
+    title: 'Michael Irigoyen - Downloading 1UP: Empowering Communities with APIs.pptx',
+    url: '/presentations/1up-empowering-communities-with-apis'
+  },
+  {
+    title: 'Michael Irigoyen - Downloading Design Your API for Humans.pptx',
+    url: '/presentations/design-your-api-for-humans'
+  },
+  {
+    title: 'Michael Irigoyen - Downloading The API User Experience.pptx',
+    url: '/presentations/the-api-user-experience'
+  },
+  {
+    title: 'Michael Irigoyen - Downloading Continuous DevOps.pptx',
+    url: '/presentations/continuous-devops'
+  },
+  {
+    title: 'Michael Irigoyen - Downloading Nobody Cares About Your UI.pptx',
+    url: '/presentations/nobody-cares-about-your-ui'
+  }
+]);
