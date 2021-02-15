09db826dadf1421b9077ccb990675dbb78d22bac
Michael Irigoyen <mirigoyen@accusoft.com>
2021-02-15 10:01:25 -0600

    Update talks locations in Git

--- src/Router.js
+++ src/Router.js
@@ -59,29 +59,29 @@ const Router = () => {
           file='/Michael%20Irigoyen%20Resume.pdf'
         />
         <Download
-          name='1UP: Empowering Communities with APIs.pptx'
+          name='1UP - Empowering Communities with APIs.pptx'
           path='/presentations/1up-empowering-communities-with-apis'
-          file='https://github.com/goyney/talks/blob/main/1UP_%20Empowering%20Communities%20with%20APIs.pptx?raw=true'
+          file='https://github.com/goyney/talks/blob/main/1UP%20-%20Empowering%20Communities%20with%20APIs/1UP%20-%20Empowering%20Communities%20with%20APIs.pptx?raw=true'
         />
         <Download
           name='Design Your API for Humans.pptx'
           path='/presentations/design-your-api-for-humans'
-          file='https://github.com/goyney/talks/blob/main/Design%20Your%20API%20for%20Humans.pptx?raw=true'
+          file='https://github.com/goyney/talks/blob/main/Design%20Your%20API%20for%20Humans/Design%20Your%20API%20for%20Humans.pptx?raw=true'
         />
         <Download
           name='The API User Experience.pptx'
           path='/presentations/the-api-user-experience'
-          file='https://github.com/goyney/talks/blob/main/The%20API%20User%20Experience.pptx?raw=true'
+          file='https://github.com/goyney/talks/blob/main/The%20API%20User%20Experience/The%20API%20User%20Experience.pptx?raw=true'
         />
         <Download
           name='Continuous DevOps.pptx'
           path='/presentations/continuous-devops'
-          file='https://github.com/goyney/talks/blob/main/Continuous%20DevOps.pptx?raw=true'
+          file='https://github.com/goyney/talks/blob/main/Continuous%20DevOps/Continuous%20DevOps.pptx?raw=true'
         />
         <Download
           name='Nobody Cares About Your UI.pptx'
           path='/presentations/nobody-cares-about-your-ui'
-          file='https://github.com/goyney/talks/blob/main/FED%20UP%20-%20Nobody%20Cares%20About%20Your%20UI.pptx?raw=true'
+          file='https://github.com/goyney/talks/blob/main/Nobody%20Cares%20About%20Your%20UI/Nobody%20Cares%20About%20Your%20UI.pptx?raw=true'
         />
 
         <ErrorPage default path='/404' />
