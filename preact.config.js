6ef36bd875d196a1bb0c7a20dc17cc747afda2cf
Michael Irigoyen <michael@irigoyen.dev>
2021-07-28 13:21:30 -0500

    Update deps, add Polywork

--- preact.config.js
+++ preact.config.js
@@ -18,6 +18,21 @@ export default (config) => {
   const cssClassIdentName = process.env.BUILD_ID ? '[hash:base64:5]' : '[path][name]__[local]';
   config.module.rules[4].use[1].options.modules.localIdentName = cssClassIdentName;
 
+  // Handle loading SVGs as elements
+  config.module.rules[7].test = /\.(woff2?|ttf|eot|jpe?g|png|webp|gif|mp4|mov|ogg|webm)(\?.*)?$/i;
+  config.module.rules.push({
+    exclude: /node_modules/,
+    loader: 'react-svg-loader',
+    options: {
+      svgo: {
+        plugins: [
+          { collapseGroups: false }
+        ]
+      }
+    },
+    test: /\.svg$/
+  });
+
   // Add an alias to the assets directory
   config.resolve.alias.assets = path.resolve(__dirname, './src/assets/');
 
