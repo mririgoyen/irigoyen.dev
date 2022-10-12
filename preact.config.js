1c7c502e287d21c0add4153523f8504132af39b1
Michael Irigoyen <michael@irigoyen.dev>
2022-10-11 22:36:43 -0500

    Move some css around in an attempt to prevent the critters error

--- preact.config.js
+++ preact.config.js
@@ -4,7 +4,7 @@ import RobotstxtPlugin from 'robotstxt-webpack-plugin';
 
 import { baseUrl } from './build/defaults';
 
-export default (config, env, helpers) => {
+export default (config) => {
   // Set global variables
   config.plugins.push(
     new webpack.DefinePlugin({
@@ -17,13 +17,6 @@ export default (config, env, helpers) => {
   // Override CSS modules class names
   const cssClassIdentName = process.env.BUILD_ID ? '[hash:base64:5]' : '[path][name]__[local]';
   config.module.rules[4].use[1].options.modules.localIdentName = cssClassIdentName;
-
-  // Don't preload keyframes since they're causing Netlify build to blow up
-  // TODO: Investigate this later
-  const critters = helpers.getPluginsByName(config, 'Critters')[0];
-  if (critters) {
-    critters.plugin.options.keyframes = 'none';
-  }
 
   // Handle loading SVGs as elements
   config.module.rules[7].test = /\.(woff2?|ttf|eot|jpe?g|png|webp|gif|mp4|mov|ogg|webm)(\?.*)?$/i;
