13bf36dae5c70b48a3849cc21d8abc42b6bb06c1
Michael Irigoyen <michael@irigoyen.dev>
2022-12-20 14:25:47 -0600

    Add webfinger for Mastodon

--- build/build.js
+++ build/build.js
@@ -2,10 +2,12 @@ const staticRoutes = require('./prerender/getStaticRoutes');
 const getBlogRoutes = require('./prerender/getBlogRoutes');
 const generateSitemap = require('./sitemap/generateSitemap');
 const generateRss = require('./syndication/generateRss');
+const generateWebfinger = require('./webfinger/generateWebfinger');
 
 const blogRoutes = getBlogRoutes();
 const prerenderedRoutes = staticRoutes.concat(blogRoutes);
 generateSitemap(prerenderedRoutes);
 generateRss(blogRoutes);
+generateWebfinger();
 
 module.exports = prerenderedRoutes;
---
+++ build/webfinger/generateWebfinger.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const generateWebfinger = () => {
+  const webfinger = {
+    aliases: [
+      'https://hachyderm.io/@mririgoyen',
+      'https://hachyderm.io/users/mririgoyen'
+    ],
+    links: [
+      {
+        href: 'https://hachyderm.io/@mririgoyen',
+        rel: 'http://webfinger.net/rel/profile-page',
+        type: 'text/html'
+      },
+      {
+        href: 'https://hachyderm.io/users/mririgoyen',
+        rel: 'self',
+        type: 'application/activity+json'
+      },
+      {
+        rel: 'http://ostatus.org/schema/1.0/subscribe',
+        template: 'https://hachyderm.io/authorize_interaction?uri={uri}'
+      }
+    ],
+    subject: 'acct:mririgoyen@hachyderm.io'
+  };
+
+  fs.writeFileSync('./dist/.well-known/webfinger', JSON.stringify(webfinger));
+};
+
+module.exports = generateWebfinger;
