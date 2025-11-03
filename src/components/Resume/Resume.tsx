e09b8c78e2b34d19d8d9b6070561666a2b80ac60
Michael Irigoyen <michael@irigoyen.dev>
2025-11-03 10:09:49 -0600

    Update job history

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -37,7 +37,7 @@ const Resume: FunctionComponent<ResumeProps> = ({ setActiveSection }) => {
           </div>
 
           <div className={classes.item}>
-            <h3>Hearst Magazines<span>Formerly MotorTrend Group</span></h3>
+            <h3>MotorTrend Group</h3>
             <p
               aria-level='4'
               className={classes.subheading}
@@ -45,9 +45,9 @@ const Resume: FunctionComponent<ResumeProps> = ({ setActiveSection }) => {
             >
               <em>UX Engineer</em>
               <span className={classes.bullet}>&bull;</span>
-              <span>April 2023 - Current</span>
+              <span>April 2023 - November 2025</span>
             </p>
-            <p>As a UX Engineer, I am the architect and lead engineer of MotorTrend's React component library, Ignition Design System. My responsibilities have been to work closely with the UX group, product management, and engineering to build and provide reusable, high quality components to make building our web presence easy and predictable. Our component library is built to be responsive, accessible, and to take advantage of features of Next.js, including Server-Side Rendering (SSR).</p>
+            <p>As a UX Engineer, I was the architect and lead engineer of MotorTrend's React component library, Ignition Design System. My responsibilities included working closely with the UX group, product management, and engineering to build and provide reusable, high quality components to make building our web presence easy and predictable. Our component library was built to be responsive, accessible, and to take advantage of features of Next.js, including Server-Side Rendering (SSR).</p>
           </div>
 
           <div className={classes.item}>
